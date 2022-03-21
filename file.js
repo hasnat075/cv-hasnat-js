475cabf274fe02aa9341a14d502230ca9326ec81
Muhammad Taimoor <98056763+hasnat075@users.noreply.github.com>
2022-03-21 19:13:46 +0500

    Attaching JS and Bootstrap

---
+++ file.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $(".arrow-p").click(function(){
+        $(".content").slideToggle("slow");
+    });
+
+    $(".arrow-s").click(function(){
+        $(".skills").slideToggle("slow");
+    });
+
+    $(".arrow-e").click(function(){
+        $(".experience").slideToggle("slow");
+    });
+
+    $(".arrow-ed").click(function(){
+        $(".education").slideToggle("slow");
+    });
+
+    $(".arrow-l").click(function(){
+        $(".languages").slideToggle("slow");
+    });
+
+    
+});
